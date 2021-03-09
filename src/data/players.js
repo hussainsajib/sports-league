101e0d1d3ce72a82d30a33dbe99539da6864ce6f
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-03-08 21:25:30 -0500

    dummy data

---
+++ src/data/players.js
@@ -0,0 +1,42 @@
+export const players = [
+    {
+        name: 'player one',
+        team: 'team one'
+    },
+    {
+        name: 'player two',
+        team: 'team one'
+    },
+    {
+        name: 'player three',
+        team: 'team two'
+    },
+    {
+        name: 'player four',
+        team: 'team two'
+    },
+    {
+        name: 'player five',
+        team: 'team three'
+    },
+    {
+        name: 'player six',
+        team: 'team three'
+    },
+    {
+        name: 'player seven',
+        team: 'team four'
+    },
+    {
+        name: 'player eight',
+        team: 'team four'
+    },
+    {
+        name: 'player nine',
+        team: 'team five'
+    },
+    {
+        name: 'player ten',
+        team: 'team five'
+    }
+]
---
+++ src/data/teams.js
@@ -0,0 +1,22 @@
+export const teams = [
+    {
+        name: 'Team One',
+        players: ['player one', 'player two']
+    },
+    {
+        name: 'Team two',
+        players: [ 'player three', 'player four' ]
+    },
+    {
+        name: 'team three',
+        players: [ 'player five', 'player six' ]
+    },
+    {
+        name: 'team four',
+        players: [ 'player seven', 'player eight' ]
+    },
+    {
+        name: 'team five',
+        players: [ 'player nine', 'player ten' ]
+    }
+]
